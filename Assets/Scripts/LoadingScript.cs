45b1390d5cf5462dc883949334917b09417028c9
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2020-02-15 16:11:06 +0100

    ...

---
+++ Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,27 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour
+{
+    public GameObject Slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadGame());
+    }
+    private IEnumerator LoadGame()
+    {
+        //loads the main game
+        AsyncOperation Loading = SceneManager.LoadSceneAsync("Game");
+
+        while (!Loading.isDone)
+        {
+            float progress = Mathf.Clamp01(Loading.progress / .9f);
+            Slider.GetComponent<Slider>().value = progress;
+            yield return 0;
+        }
+    }
+}
