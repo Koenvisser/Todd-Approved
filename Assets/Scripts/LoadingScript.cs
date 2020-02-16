5fee3aba8c9795a93822a39a9c13f69a46183ead
Koenvisser <57984909+Koenvisser@users.noreply.github.com>
2020-02-16 15:22:45 +0100

    ....

--- Assets/Scripts/LoadingScript.cs
+++ Assets/Scripts/LoadingScript.cs
@@ -16,17 +16,15 @@ public class LoadingScript : MonoBehaviour
     private IEnumerator LoadGame()
     {
         //loads the main game
+        yield return new WaitForSeconds(.1f);
         AsyncOperation Loading = SceneManager.LoadSceneAsync("Game");
-        float progress;
         Loading.allowSceneActivation = false;
         while (Loading.progress < .9f)
         {
-            progress = Mathf.Clamp01(Loading.progress / .9f);
-            Slider.GetComponent<Slider>().value = progress;
+            Slider.GetComponent<Slider>().value = Mathf.Clamp01(Loading.progress / .9f);
             yield return 0;
         }
-        progress = Mathf.Clamp01(Loading.progress / .9f);
-        Slider.GetComponent<Slider>().value = progress;
+        Slider.GetComponent<Slider>().value = Mathf.Clamp01(Loading.progress / .9f);
         animator.SetTrigger("FadeOutTrigger");
         yield return new WaitForSeconds(1);
         Loading.allowSceneActivation = true;
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Quit()
+    {
+        StartCoroutine(Quit2());
+    }
+
+    private IEnumerator Quit2() {
+        animator.SetTrigger("FadeOutTrigger");
+        yield return new WaitForSeconds(1);
+        Application.Quit();
+    }
+}
